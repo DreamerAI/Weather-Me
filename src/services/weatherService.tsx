862ab0f22c50d7829795fa9a8f14405f72e9954a
Aidar <abdykayimovaidar@gmail.com>
2023-03-17 23:17:57 +0300

    Global update

--- src/services/weatherService.tsx
+++ src/services/weatherService.tsx
@@ -3,6 +3,7 @@ import api from "../axios";
 import { Weather, WeeklyForecast } from "../store/types/types";
 
 export class WeatherService {
+
     static getCurrentWeather(city: string): Promise<AxiosResponse<Weather>> {
         return api.get<Weather>(`/weather?q=${city}`)
     }
